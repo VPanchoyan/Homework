fc20681458cb20a78e35405cec9473c63b703850
VPanchoyan <114210044+VPanchoyan@users.noreply.github.com>
2022-09-24 15:31:09 +0400

    Delete task2.py

--- task2.py
+++
@@ -1,7 +0,0 @@
-# Is input consonant or vowel
-
-char = input("Enter a character: ")
-if len(char) == 1 and char.isalpha():
-    print(char,"is vowel.") if char in "AaEeIiOoUu" else print(char,"is consonant.")
-else:
-    print("Enter one character from alphabet!")
