2552df626960c007ba76c24e5bdffea9d537cb2d
VPanchoyan <114210044+VPanchoyan@users.noreply.github.com>
2022-10-03 17:58:56 +0400

    Create task7.py

---
+++ Homework3/task7.py
@@ -0,0 +1,7 @@
+l = [[1,2,3,4,5],[2,3,4,5,6],[3,4,5,6,7],[4,5,6,7,8],[5,6,7,8,9]]
+flat_list = [i  for sublist in l for i in sublist]
+dic = {}
+for i in l:
+    for j in i:
+        dic[j] = flat_list.count(j)
+dic = sorted(dic.items(),key = dic.values())
